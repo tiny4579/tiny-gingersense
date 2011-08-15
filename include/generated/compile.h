f55f4c696d945c623e6d94b8bcf57e6b25fbf1f5
Andrew <andrewb03@gmail.com>
2011-08-14 22:53:16 -0400

    initial kernel build

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 16 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#16 PREEMPT Sun Aug 14 19:23:46 EDT 2011"
+#define LINUX_COMPILE_TIME "19:23:46"
+#define LINUX_COMPILE_BY "drew"
+#define LINUX_COMPILE_HOST "tinster"
+#define LINUX_COMPILE_DOMAIN "localdomain"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
