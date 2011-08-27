ceca3bb5c3b714b80395674740563821c6b7b2c7
Andrew <andrewb03@gmail.com>
2011-08-27 19:25:34 -0400

    working build, see inc-config

--- drivers/gpu/drm/i2c/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/gpu/drm/i2c/built-in.o :=  rm -f drivers/gpu/drm/i2c/built-in.o; /home/drew/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-ar rcs drivers/gpu/drm/i2c/built-in.o
--- fs/fuse/.fuse.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_fs/fuse/fuse.o := /home/drew/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-ld -EL    -r -o fs/fuse/fuse.o fs/fuse/dev.o fs/fuse/dir.o fs/fuse/file.o fs/fuse/inode.o fs/fuse/control.o 
