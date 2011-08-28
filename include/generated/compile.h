8dd5664bd0054bc4fe7c27fd5f8995f912e75dc1
Andrew <andrewb03@gmail.com>
2011-08-28 14:06:25 -0400

    fixed vibration+lowered wifi voltage

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 25 */
+/* This file is auto generated, version 27 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#25 PREEMPT Sat Aug 27 23:47:48 EDT 2011"
-#define LINUX_COMPILE_TIME "23:47:48"
+#define UTS_VERSION "#27 PREEMPT Sun Aug 28 10:19:38 EDT 2011"
+#define LINUX_COMPILE_TIME "10:19:38"
 #define LINUX_COMPILE_BY "drew"
 #define LINUX_COMPILE_HOST "tinster"
 #define LINUX_COMPILE_DOMAIN "localdomain"
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.10-gceca3bb-dirty"
+#define UTS_RELEASE "2.6.35.10-gb632f55-dirty"
