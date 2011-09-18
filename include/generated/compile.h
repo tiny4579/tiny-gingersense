298c5fe3d5e36c16eb6e2e55f20a967462138833
Andrew <andrewb03@gmail.com>
2011-09-18 13:34:12 -0400

    Add support for swap and compcache

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 53 */
+/* This file is auto generated, version 57 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#53 PREEMPT Thu Sep 1 21:54:53 EDT 2011"
-#define LINUX_COMPILE_TIME "21:54:53"
+#define UTS_VERSION "#57 PREEMPT Fri Sep 16 17:03:28 EDT 2011"
+#define LINUX_COMPILE_TIME "17:03:28"
 #define LINUX_COMPILE_BY "drew"
 #define LINUX_COMPILE_HOST "tinster"
-#define LINUX_COMPILE_DOMAIN "localdomain"
-#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
